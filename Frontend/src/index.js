f508a893c3c464d73be70fcf884f75de9b8f912a
bilalchannar <188172290+bilalchannar@users.noreply.github.com>
2026-04-25 12:05:18 +0500

    Delete Frontend directory

--- Frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AppRoutes from "./Routes/AppRoutes.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppRoutes />);
